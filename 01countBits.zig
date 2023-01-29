18749f81baaff58e6d3e420d998d47beff5319d7
Roman Frołow <rofrol@gmail.com>
2023-01-29 16:27:44 +0100

    01countBits.zig, util.zig

--- 01countBits.zig
+++ 01countBits.zig
@@ -1,9 +1,13 @@
 const std = @import("std");
 const assert = std.debug.assert;
+const expect = std.testing.expect;
+const util = @import("util.zig");
+const expectEq = util.expectEq;
+
 fn countBits(comptime T: type) T {
     return 2;
 }
 
 test "countBits" {
-    assert(true);
+    try expectEq(true, true);
 }
---
+++ util.zig
@@ -0,0 +1,8 @@
+const std = @import("std");
+
+// https://github.com/ziglang/zig/issues/4437#issuecomment-683309291
+// https://github.com/ziglang/zig/issues/4437#issuecomment-585408987
+// https://github.com/zigimg/zigimg/blob/e57148bf6c6df395ef308e559ec833639940220c/tests/helpers.zig#LL14C5-L16C2
+pub fn expectEq(expected: anytype, actual: anytype) !void {
+    try std.testing.expectEqual(@as(@TypeOf(actual), expected), actual);
+}
