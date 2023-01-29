8b0a08dc5f01308fef55421181339904eedffc2c
Roman Frołow <rofrol@gmail.com>
2023-01-29 17:37:03 +0100

    01countBits.zig

--- 01countBits.zig
+++ 01countBits.zig
@@ -1,11 +1,20 @@
-const std = @import("std");
 const util = @import("util.zig");
 const expectEq = util.expectEq;
 
-fn countBits(comptime T: type) T {
-    return 2;
+fn countSetBits(
+    comptime T: type,
+    val: T,
+) T {
+    var value = val;
+    var count: T = 0;
+    while (value != 0) : (count += 1) {
+        value = value & (value - 1);
+    }
+    return count;
 }
 
 test "countBits" {
-    try expectEq(true, true);
+    try expectEq(1, countSetBits(u32, 1));
+    try expectEq(1, countSetBits(u32, 2));
+    try expectEq(2, countSetBits(u32, 3));
 }
